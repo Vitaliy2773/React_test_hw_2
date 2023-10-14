35f96d89393720fec4f6d90b12c160b648ed67aa
Vitaliy2773 <vitalii64773@gmail.com>
2023-10-13 21:34:46 -1100

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import React from 'react';
+import Counter from './Counter/Counter';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Стан компонента</h1>
+
+      <Counter />
+
+      {/* <Colorpicker options={colorPickerOptions} /> */}
+
+      {/* <Dropdown /> */}
+    </>
   );
 };
+
+export default App;
